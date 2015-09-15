1f1a2c94b3f3d21a33eb6eb50bf7e8fca3aa0565
pooleside <lpoole26@gmail.com>
2015-09-15 12:23:09 -0700

    "all unit tests passing"

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ var isatriangle = function(side1, side2, side3) {
     return true;
   }
 }
+var triangletype = function(side1, side2, side3) {
+  if ((side1 === side2) && (side1 === side3) && (side2 === side3)) {
+    return "equilateral";
+  } else if ((side1 === side2) || (side1 === side3) || (side2 === side3)) {
+    return "isosceles"
+  } else  {
+    return "scalene"
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,18 @@ describe('isatriangle', function() {
     expect(isatriangle(6,4,4)).to.equal(true);
   });
 });
+
+describe('triangletype', function() {
+  it("all sides of triangle are equal in length, it is equilateral", function() {
+    expect(triangletype(8,8,8)).to.equal('equilateral');
+  });
+
+  it("two sides of triangle are equal in length, it is isosceles", function() {
+    expect(triangletype(8,8,6)).to.equal('isosceles');
+  });
+
+  it("no sides of triangle are equal in length, it is scalene", function() {
+    expect(triangletype(7,8,6)).to.equal('scalene');
+  });
+
+});
