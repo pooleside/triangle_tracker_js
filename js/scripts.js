3234a4600453cb63365583a2bdd06b7fab5e26a7
pooleside <lpoole26@gmail.com>
2015-09-15 14:19:51 -0700

    "add user interface"

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,24 @@ var triangletype = function(side1, side2, side3) {
     return "scalene"
   }
 }
+
+$(document).ready(function() {
+  $("form#triangle-tracker").submit(function(event) {
+    var side1= parseInt($("input#side1").val());
+    var side2= parseInt($("input#side2").val());
+    var side3= parseInt($("input#side3").val());
+    var result1 = isatriangle(side1, side2, side3);
+    var result2 = triangletype(side1, side2, side3);
+
+
+
+
+    if(!result1) {
+      $(".not").text("do not") &&  $("#result1").show() && $("#result2").hide();
+    }
+    else {
+      $(".not").text("") && $(".type").text(result2) && $("#result1").show()  && $("#result2").show();
+    }
+    event.preventDefault();
+  });
+});
